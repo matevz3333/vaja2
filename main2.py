9c0e52e85479f873f457596587d9396e111d52cf
matevz3333 <70287192+matevz3333@users.noreply.github.com>
2025-09-17 17:41:17 +0000

    celeste-random-map-api

--- main2.py
+++ main2.py
@@ -6,8 +6,6 @@ def get_random_celeste_map_link():
 	klic = requests.get(url)
 	klic.raise_for_status()
 	soup = BeautifulSoup(klic.text, "html.parser")
-	# Find the first <a> tag whose href contains 'gamebanana.com/mods/'
-	# Try to extract the map link from the meta tag 'og:url'
 	meta_tag = soup.find('meta', property='og:url')
 	if meta_tag and meta_tag.get('content'):
 		print(meta_tag['content'])
